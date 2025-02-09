3c8f7d52c7cb5426765a8851a3934e4b548c1940
Abraham Ajibade <97408939+jibbs1703@users.noreply.github.com>
2025-02-09 05:16:05 -0500

    added visualization help module, completed extraction module and got started on feature engineering

--- src/adult_income/etl/extract.py
+++ src/adult_income/etl/extract.py
@@ -1,1 +1,16 @@
 """module for data extraction."""
+import pandas as pd
+from utils.aws.s3 import S3Buckets
+from utils.helpers.etl_helpers import extract_csv
+
+def run_data_extraction(bucket_name: str, file_name: str) -> pd.DataFrame:
+    """function to run data extraction."""
+    s3_conn = S3Buckets.credentials()
+    csv_file = s3_conn.read_file(bucket_name, file_name)
+    return extract_csv(csv_file)
+
+if __name__ == "__main__":
+    BUCKET_NAME = "jibbs-raw-datasets"
+    FILE_NAME = "uncleanedAdultIncome.csv"
+    df = run_data_extraction(BUCKET_NAME, FILE_NAME)
+    print(df.head())
